16c226cd161a0d2bd2adff3f20dfb56839613a15
Elliot Liu (liuliu) <liuliu@cisco.com>
2019-10-28 18:25:54 -0700

    finished the buttons with chain from https://codelabs.developers.google.com/codelabs/kotlin-android-training-constraint-layout/index.html#15

--- app/src/main/java/com/aisnote/android/colormyviews/MainActivity.kt
+++ app/src/main/java/com/aisnote/android/colormyviews/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +24,9 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.green_button -> box_four_text.setBackgroundResource(R.color.my_green)
+            R.id.yellow_button -> box_five_text.setBackgroundResource(R.color.my_yellow)
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
@@ -36,7 +40,13 @@ class MainActivity : AppCompatActivity() {
 
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
 
-        val clickableViews: List<View> = listOf(boxOneText, boxTwoText, boxThreeText, boxFourText, boxFiveText, rootConstraintLayout)
+        val redButton = findViewById<TextView>(R.id.red_button)
+        val greenButton = findViewById<TextView>(R.id.green_button)
+        val yellowButton = findViewById<TextView>(R.id.yellow_button)
+
+
+        val clickableViews: List<View> = listOf(boxOneText, boxTwoText, boxThreeText, boxFourText, boxFiveText, rootConstraintLayout,
+            redButton, greenButton, yellowButton)
 
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(it)}
